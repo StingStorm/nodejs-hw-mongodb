b7f09e623378a5ff3e605a38455e8658609a175d
StingStorm <alexkalganov18@gmail.com>
2024-12-09 20:06:37 +0200

    changed isValidId error message text

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,10 @@ export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
 
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Bad Request');
+    throw createHttpError(
+      400,
+      'ValidationError: The contact ID is not a valid object',
+    );
   }
 
   next();
